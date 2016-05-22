75e9a69ac768ea938fb05ebe412eb2029f620e26
Michaelmvv <bukkit.michaelmvv@gmail.com>
2016-05-21 22:31:31 -0700

    Change Stuffs

--- client/main.js
+++ client/main.js
@@ -3,3 +3,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 Meteor.subscribe("times");
+
+Template.main.events({
+  "click #MakeNew": function(e){
+    Router.go("/cal");
+  }
+});
+
+Template.registerHelper("name", function(){
+return ProjectName;
+});
